6d47bdf9a8adda11d3a711dfb1f36a607191b306
Samuel James Clark <samueljclark@Samuels-MacBook-Pro.local>
2021-02-21 19:06:18 -0600

    histogram output added

--- am_05.py
+++ am_05.py
@@ -1,47 +1,3 @@
-# #########################################################################
-# Copyright (c) 2020, UChicago Argonne, LLC. All rights reserved.         #
-#                                                                         #
-# Copyright 2019-2020. UChicago Argonne, LLC. This software was produced  #
-# under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
-# Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
-# U.S. Department of Energy. The U.S. Government has rights to use,       #
-# reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR    #
-# UChicago Argonne, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR        #
-# ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is     #
-# modified to produce derivative works, such modified software should     #
-# be clearly marked, so as not to confuse it with the version available   #
-# from ANL.                                                               #
-#                                                                         #
-# Additionally, redistribution and use in source and binary forms, with   #
-# or without modification, are permitted provided that the following      #
-# conditions are met:                                                     #
-#                                                                         #
-#     * Redistributions of source code must retain the above copyright    #
-#       notice, this list of conditions and the following disclaimer.     #
-#                                                                         #
-#     * Redistributions in binary form must reproduce the above copyright #
-#       notice, this list of conditions and the following disclaimer in   #
-#       the documentation and/or other materials provided with the        #
-#       distribution.                                                     #
-#                                                                         #
-#     * Neither the name of UChicago Argonne, LLC, Argonne National       #
-#       Laboratory, ANL, the U.S. Government, nor the names of its        #
-#       contributors may be used to endorse or promote products derived   #
-#       from this software without specific prior written permission.     #
-#                                                                         #
-# THIS SOFTWARE IS PROVIDED BY UChicago Argonne, LLC AND CONTRIBUTORS     #
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       #
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS       #
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UChicago     #
-# Argonne, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,        #
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,    #
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;        #
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER        #
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT      #
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN       #
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
-# POSSIBILITY OF SUCH DAMAGE.                                             #
-# #########################################################################
 
 """
 Created on Wed Feb 17 16:20:16 2021
@@ -194,10 +150,10 @@ def scale_stack(ims):
 def denoise_segmentation(segmentation):
     additive = (segmentation == 0)
     subtractive = (segmentation == 2)
-    addtive = ndi.median_filter(additive, size = (6, 6, 1))
+    additive = ndi.median_filter(additive, size = (6, 6, 1))
     subtractive = ndi.median_filter(subtractive, size = (6, 6, 1))
     segmentation = np.ones_like(segmentation)
-    segmentation[addtive] = 0
+    segmentation[additive] = 0
     segmentation[subtractive] = 2
     return segmentation
 
@@ -294,6 +250,8 @@ def main(arg):
     
     rawims = loadsynchronous(filepathin)
     
+    rawims = ndi.median_filter(rawims, size = (3, 3, 1))
+    
     clearims = np.copy(rawims[:,:,0:50])
     clearims = flicker(clearims)
     clearim = np.nanmean(clearims, axis = 2)
@@ -318,7 +276,12 @@ def main(arg):
     segmentation, keyhole_centroid = interpret_segmentation(segmentation, 
                                  skymask, keyhole_threshold)
     
-    saveims(np.uint8(segmentation),filepathin,filepathout)    
+    saveims(np.uint8(segmentation),filepathin,filepathout)
+
+    plt.hist(delta_rawims.ravel(), bins = 256, color = 'blue')
+    plt.hist(delta_clearims.ravel(), bins = 256, color = 'red')
+    plt.yscale('symlog')
+    plt.show()    
         
     slider_ims = scale_stack(segmentation)
     
